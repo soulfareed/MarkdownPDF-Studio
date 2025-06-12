49a8a84c7846bcbd7233538e0cd6d12de916b61e
Md fareed <fareedzaman501@gmail.com>
2025-06-12 21:16:31 +0530

    Auth connected

--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -7,7 +7,7 @@ import React, {
 } from "react";
 import {
   login as apiLogin,
-  register as apiRegister,
+  register as apiregister,
   logout as apiLogout,
 } from "../api/api";
 import { jwtDecode } from "jwt-decode";
@@ -37,11 +37,28 @@ export function AuthProvider({ children }: { children: ReactNode }) {
       }
     }
     setLoading(false);
-  },[]);
+  }, []);
 
-  const login = aync (email: string, password: string) => {
+  const login = async (email: string, password: string) => {
     const response = await apiLogin(email, password);
-   const decoded: any = jwtDecode(response.data.access_token);
-   setUser(decoded);
-  }
+    const decoded: any = jwtDecode(response.data.access_token);
+    setUser(decoded);
+  };
+
+  const register = async (email: string, password: string, name?: string) => {
+    const response = await apiResister(email, password, name);
+    if (response.data.access_token) {
+      const decoded: any = jwtDecode(response.data.access_token);
+      setUser(decoded);
+    }
+  };
+  const logout = async () => {
+    await apiLogout();
+    setUser(null);
+  };
+  const value = { user, loading, login, register, logout };
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
+export function useAuth() {
+  return useContext(AuthContext);
 }
